2d94edb15c794f7177f66275ee21626c86b23682
Paul-Philippe Nelles <ppnelles@gmail.com>
2024-05-29 14:24:15 +0200

    init

---
+++ themes/domainedelapothicaire/template-parts/content-favicons.php
@@ -0,0 +1,15 @@
+<?php 
+/* 
+* Use the script on https://realfavicongenerator.net/
+* Blank template in /img/favicons/favicon-master.php
+*/ ?>
+
+<?php /* 
+<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory'); ?>/img/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_directory'); ?>/img/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_directory'); ?>/img/favicons/favicon-16x16.png">
+<link rel="manifest" href="<?php bloginfo('template_directory'); ?>/img/favicons/site.webmanifest">
+<link rel="mask-icon" href="<?php bloginfo('template_directory'); ?>/img/favicons/safari-pinned-tab.svg" color="#ffffff">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="theme-color" content="#ffffff">
+*/ ?>
